e51aadf1d9275d91c65d6488fa329ac38524caff
Sachintha <snishajith@gmail.com>
2024-07-28 11:41:59 +0530

    Added Configuration Class to limit requst

---
+++ src/main/java/com/spring_security_jwt/configuration/CorsConfiguration.java
@@ -0,0 +1,29 @@
+package com.spring_security_jwt.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfiguration {
+    private static final String GET = "GET";
+    private static final String POST = "POST";
+    private static final String PUT = "PUT";
+    private static final String DELETE = "DELETE";
+
+    //meken apita limit karaganna puluwan mewata mewata access denna kiyala. meke ape okkoma tikata acccess deela thiyenne
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+            registry.addMapping("/**")
+                    .allowedMethods(GET,POST,PUT,DELETE)
+                    .allowedHeaders("*")
+                    .allowedOriginPatterns("*")
+                    .allowCredentials(true);
+            }
+        };
+    }
+}
